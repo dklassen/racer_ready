85d672f0d60531a2a4254c7e81423ab7b8cbe505
Dana Klassen <klassen.dana@gmail.com>
2022-02-14 17:09:31 -0500

    Align number and text values in the table

--- src/ResultsTable.tsx
+++ src/ResultsTable.tsx
@@ -10,50 +10,59 @@ import { SetStateAction, useState } from 'react';
 import { TableSortLabel } from '@mui/material';
 
 type SortDirection = 'DESCENDING' | 'ASCENDING';
+type TableRowAlignment = "inherit" | "left" | "center" | "right" | "justify" | undefined
 
 interface IHeader {
     id: string
     label: string
     sortBy: string
+    align: TableRowAlignment
 }
 
-const tableHeaders = [
-    {
+const RecordToColumnMap: Record<string, IHeader> = {
+    "Bib": {
         id: "Bib",
         label: "Bib #",
         sortBy: "Bib",
+        align: "left"
     },
-    {
+    "Name": {
         id: "Name",
         label: "Name",
         sortBy: "Name",
+        align: "left",
     },
-    {
+    "Club": {
         id: "Club",
         label: "Club",
         sortBy: "Club",
+        align: "left",
     },
-    {
+    "Class": {
         id: "Class",
         label: "Class",
         sortBy: "Class",
+        align: "left"
     },
-    {
+    "Run1": {
         id: "Run1",
         label: "First Run",
-        sortBy: "Run1ms"
+        sortBy: "Run1ms",
+        align: "right"
     },
-    {
+    "Run2": {
         id: "Run2",
         label: "Second Run",
-        sortBy: "Run2ms"
+        sortBy: "Run2ms",
+        align: "right",
     },
-    {
+    "TotalTime": {
         id: "TotalTime",
         label: "Total Time",
         sortBy: "TotalTimems",
+        align: "right"
     }
-]
+}
 
 type ObjectSortFunc = <T, K extends keyof T>(a: T, b: T, orderField: K) => number
 
@@ -108,10 +117,10 @@ function ResultsTable({ racers, sortFunc }: Props) {
                     <TableHead>
                         <TableRow>
                             {
-                                tableHeaders.map((header: IHeader) => (
+                                Object.values(RecordToColumnMap).map((header: IHeader) => (
                                     <TableCell
                                         key={header.id}
-                                        align="right">
+                                        align={header.align}>
                                         <TableSortLabel
                                             active={orderBy === header.id}
                                             direction={orderBy === header.id ? order as any : 'asc'}
@@ -119,7 +128,6 @@ function ResultsTable({ racers, sortFunc }: Props) {
                                         >
                                             {header.label}
                                         </TableSortLabel>
-
                                     </TableCell>
                                 ))}
                         </TableRow>
@@ -131,18 +139,18 @@ function ResultsTable({ racers, sortFunc }: Props) {
                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                             >
                                 <TableCell component="th" scope="row">{row.Bib} </TableCell>
-                                <TableCell align="right">{row.Name}</TableCell>
-                                <TableCell align="right">{row.Club}</TableCell>
-                                <TableCell align="right">{row.Class}</TableCell>
-                                <TableCell align="right">{row.Run1}</TableCell>
-                                <TableCell align="right">{row.Run2}</TableCell>
-                                <TableCell align="right">{row.TotalTime}</TableCell>
+                                <TableCell align={RecordToColumnMap.Bib.align}>{row.Name}</TableCell>
+                                <TableCell align={RecordToColumnMap.Club.align}>{row.Club}</TableCell>
+                                <TableCell align={RecordToColumnMap.Class.align}>{row.Class}</TableCell>
+                                <TableCell align={RecordToColumnMap.Run1.align}>{row.Run1}</TableCell>
+                                <TableCell align={RecordToColumnMap.Run2.align}>{row.Run2}</TableCell>
+                                <TableCell align={RecordToColumnMap.TotalTime.align}>{row.TotalTime}</TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
                 </Table>
             </TableContainer>
-        </Paper>
+        </Paper >
     );
 }
 
